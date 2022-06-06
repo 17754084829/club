18017d0e350e684819ecffce10ffb9e3b020974b
jinggege <1878618369@qq.com>
2022-06-06 23:14:49 +0800

    second

--- src/main/java/club/main/Main.java
+++ src/main/java/club/main/Main.java
@@ -4,12 +4,11 @@ import club.dao.ClubInfoDao;
 import club.dao.TeacherDao;
 import club.model.ClubInfo;
 import club.util.MybatiesUtils;
+import club.weight.element.Table;
 import org.apache.ibatis.session.SqlSession;
 
 public class Main {
     public static void main(String[] args) {
-        SqlSession sqlSession= MybatiesUtils.getSqlSession();
-        TeacherDao teacherDao=sqlSession.getMapper(TeacherDao.class);
-        System.out.println(teacherDao.getList(null));
+        new Table();
     }
 }
